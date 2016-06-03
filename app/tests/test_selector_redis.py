ead686f505664e563f28d963cfb4b89d6dd48c3c
eronde <ghe@nemo-site.eu>
2016-06-03 14:11:45 +0200

    Clean up code

--- app/tests/test_selector_redis.py
+++ app/tests/test_selector_redis.py
@@ -1,16 +1,18 @@
 import unittest
-from app.util.Selector_redis import *
+import re
 import fakeredis
+from app.util.Selector_redis import Selector_redis, SelectorRedisNoBaseKeyFoundError,SelectorRedisNoSuggestWordFoundError, SelectorRedisError, SelectorRedisEmptyValue
 
-class GramTest(unittest.TestCase):
-    """Docstring for GramTest. """
+
+class Selector_redisTest(unittest.TestCase):
+    """Docstring for Selector_redisTest. """
 
     def setUp(self):
         """ Selector_redis:Set up the fakeredis
         """
         self.redis = fakeredis.FakeRedis()
-        self.redis.zadd("Hello","Alice", 1)
-        self.redis.zadd("Hello","Bob", 10)
+        self.redis.zadd("Hello",  "Alice", 1)
+        self.redis.zadd("Hello", "Bob", 10)
 
     def tearDown(self):
         self.redis.flushall()
@@ -20,28 +22,28 @@ class GramTest(unittest.TestCase):
         :returns: TODO
         """
         obj = Selector_redis(self.redis)
-        x=obj.gen_fetchWords('Hello',withscore=True)
-        match = [('Bob'.encode(),10), ('Alice'.encode(),1)]
-        self.assertListEqual(list(x),match)
-    
+        x = obj.gen_fetchWords('Hello', withscore=True)
+        match = [('Bob'.encode(), 10), ('Alice'.encode(), 1)]
+        self.assertListEqual(list(x), match)
+
     def test_fetchSuggestedWordsWithoutScores(self):
         """Selector_redis: Fetch all suggested words of words 'Hello' without scores
         :returns: TODO
         """
         obj = Selector_redis(self.redis)
-        x=obj.gen_fetchWords('Hello')
+        x = obj.gen_fetchWords('Hello')
         match = ['Bob'.encode(), 'Alice'.encode()]
-        self.assertListEqual(list(x),match)
-    
+        self.assertListEqual(list(x), match)
+
     def test_getSuggestedWords(self):
         """Selector_redis: Get all suggested words of words 'Hello'
         :returns: TODO
         """
         obj = Selector_redis(self.redis)
-        x=self.redis.zrevrange('Hello',0,-1)
+        x = self.redis.zrevrange('Hello', 0, -1)
         match = ['Bob', 'Alice']
-        self.assertListEqual(list(obj.gen_suggestWord(*x)),match)
-    
+        self.assertListEqual(list(obj.gen_suggestWord(*x)), match)
+
     def test_getInvalidKey(self):
         """Selector_redis: Fetch invalid base key
         :returns: TODO
@@ -59,12 +61,12 @@ class GramTest(unittest.TestCase):
         """
         obj = Selector_redis(self.redis)
         match = ['Bob', 'Alice']
-        x=self.redis.zrevrange('Hello',0,-1)
-        self.assertListEqual(list(obj.gen_suggestWord(*x)),match)
+        x = self.redis.zrevrange('Hello', 0, -1)
+        self.assertListEqual(list(obj.gen_suggestWord(*x)), match)
         match = ['Bob', 'Max', 'Alice']
-        obj.addNewSuggestWord('Hello','Max')
-        x=self.redis.zrevrange('Hello',0,-1)
-        self.assertListEqual(list(obj.gen_suggestWord(*x)),match)
+        obj.addNewSuggestWord('Hello', 'Max')
+        x = self.redis.zrevrange('Hello', 0, -1)
+        self.assertListEqual(list(obj.gen_suggestWord(*x)), match)
 
     def test_removeSuggestedWord(self):
         """Selector_redis: Remove suggested word from 'Hello' key
@@ -73,12 +75,12 @@ class GramTest(unittest.TestCase):
         """
         obj = Selector_redis(self.redis)
         match = ['Bob']
-        obj.removeSuggestWord('Hello','Alice')
-        x=self.redis.zrevrange('Hello',0,-1)
-        self.assertListEqual(list(obj.gen_suggestWord(*x)),match)
+        obj.removeSuggestWord('Hello', 'Alice')
+        x = self.redis.zrevrange('Hello', 0, -1)
+        self.assertListEqual(list(obj.gen_suggestWord(*x)), match)
 
     def test_raiseNotConnection(self):
-        """Selector_redis: Raise error if redis server is not connected 
+        """Selector_redis: Raise error if redis server is not connected$
         :returns: TODO
         """
         with self.assertRaises(Exception):
@@ -93,9 +95,9 @@ class GramTest(unittest.TestCase):
         """
         obj = Selector_redis(self.redis)
         exist = obj.existBaseKey('Hello')
-        self.assertIs(exist,True)
+        self.assertIs(exist, True)
         notexist = obj.existBaseKey('How')
-        self.assertIs(notexist,False)
+        self.assertIs(notexist, False)
 
     def test_increaseScoreOfSuggestedWord(self):
         """Selector_redis: Increase score of suggested word
@@ -103,34 +105,41 @@ class GramTest(unittest.TestCase):
 
         """
         obj = Selector_redis(self.redis)
-        obj.increaseScoreSuggestedWord('Hello','Alice')
-        scores = obj.gen_fetchWords('Hello',withscore=True)
-        match = [('Bob'.encode(),10), ('Alice'.encode(),2)]
-        self.assertListEqual(list(scores),match)
-        #Increase by 4
-        obj.increaseScoreSuggestedWord('Hello','Alice',4)
-        scores = obj.gen_fetchWords('Hello',withscore=True)
-        match = [('Bob'.encode(),10), ('Alice'.encode(),6)]
-        self.assertListEqual(list(scores),match)
-        #Increase by -7
-        obj.increaseScoreSuggestedWord('Hello','Alice',-7)
-        scores = obj.gen_fetchWords('Hello',withscore=True)
-        match = [('Bob'.encode(),10), ('Alice'.encode(),-1)]
-        self.assertListEqual(list(scores),match)
-        #Increase by string
+        obj.increaseScoreSuggestedWord('Hello', 'Alice')
+        scores = obj.gen_fetchWords('Hello', withscore=True)
+        match = [('Bob'.encode(), 10),  ('Alice'.encode(), 2)]
+        self.assertListEqual(list(scores), match)
+        # Increase by 4
+        obj.increaseScoreSuggestedWord('Hello', 'Alice', 4)
+        scores = obj.gen_fetchWords('Hello', withscore=True)
+        match = [('Bob'.encode(), 10),  ('Alice'.encode(), 6)]
+        self.assertListEqual(list(scores), match)
+        # Increase by -7
+        obj.increaseScoreSuggestedWord('Hello', 'Alice', -7)
+        scores = obj.gen_fetchWords('Hello', withscore=True)
+        match = [('Bob'.encode(), 10),  ('Alice'.encode(), -1)]
+        self.assertListEqual(list(scores), match)
+        # Increase by string
         with self.assertRaises(TypeError) as e:
-            obj.increaseScoreSuggestedWord('Hello','Alice','l')
-        #Increase non existing key
+            obj.increaseScoreSuggestedWord('Hello', 'Alice', 'l')
+        # Increase non existing key
         with self.assertRaises(SelectorRedisNoBaseKeyFoundError) as e:
-            obj.increaseScoreSuggestedWord('Hel','Alice',4)
+            obj.increaseScoreSuggestedWord('Hel', 'Alice', 4)
         error = str(e.exception)
         self.assertTrue("Error: key 'Hel' does not exists." in error)
-        #Increase non existing suggested word
+        # Increase non existing suggested word
         with self.assertRaises(SelectorRedisNoSuggestWordFoundError) as e:
-            obj.increaseScoreSuggestedWord('Hello','bobby',4)
+            obj.increaseScoreSuggestedWord('Hello', 'bobby',  4)
+        error = str(e.exception)
+        self.assertTrue("Error: suggested word: 'bobby' does not exists with basekey 'Hello'." in error)
+
+        # Increase non existing suggested word
+        with self.assertRaises(SelectorRedisEmptyValue) as e:
+            obj.increaseScoreSuggestedWord('Hello', '', 4)
         error = str(e.exception)
         print(error)
-        self.assertTrue("Error: suggested word: 'bobby' does not exists with basekey 'Hello'." in error)
+        regex = re.search("^Error in 'increaseScoreSuggestedWord': One of the given arguments .*\, 'Hello', '', 4\)' are empty\.$", error)
+        assert regex is not None
 
     def test_checkSuggestedWordExists(self):
         """Selector_redis: Check if suggested word exists or not exists
@@ -138,19 +147,18 @@ class GramTest(unittest.TestCase):
 
         """
         obj = Selector_redis(self.redis)
-        #list
-        data = ['test','bla']
-        self.assertTrue(obj.containing(data,'test'))
-        #sets 
-        setdata = {'test','bla'}
-        self.assertTrue(obj.containing(data,'test'))
-        #Not containing
-        self.assertFalse(obj.containing(data,'hello'))
-        #generator 
+        # list
+        data = ['test', 'bla']
+        self.assertTrue(obj.containing(data, 'test'))
+        # sets$
+        self.assertTrue(obj.containing(data, 'test'))
+        # Not containing
+        self.assertFalse(obj.containing(data, 'hello'))
+        # generator
         fetchdata = obj.gen_fetchWords('Hello')
         data = set(obj.gen_suggestWord(*fetchdata))
-        self.assertTrue(obj.containing(data,'Bob'))
-        self.assertFalse(obj.containing(data,'Bobby'))
+        self.assertTrue(obj.containing(data, 'Bob'))
+        self.assertFalse(obj.containing(data, 'Bobby'))
 
 if __name__ == '__main__':
     unittest.main()
--- app/util/Selector_redis.py
+++ app/util/Selector_redis.py
@@ -1,8 +1,14 @@
+from app.util.decorators import empty_arguments
+class SelectorRedisError(Exception): pass
+
+class SelectorRedisNoBaseKeyFoundError(SelectorRedisError): pass
+
+class SelectorRedisNoSuggestWordFoundError(SelectorRedisError): pass
+
+class SelectorRedisEmptyValue(SelectorRedisError): pass
 
 class Selector_redis(object):
-
     """ Selector_redis. Class for fetching bigram data from redis db
-    
     :Datastructure:
             sorte dset: key:value(listOfTuples), 'lang:{language}:gram:2:{word}':[({word},{freq score})]
     """
@@ -13,7 +19,7 @@ class Selector_redis(object):
         try:
             self.r.ping()
         except Exception:
-            raise Exception( "Error: Redis server is not connected. {x} must be a redis object.".format(x=redis_connection))
+            raise Exception("Error: Redis server is not connected. {x} must be a redis object.".format(x=redis_connection))
         else:
             self.redis_running = True
         finally:
@@ -25,11 +31,11 @@ class Selector_redis(object):
         :returns: Boolean
 
         """
-        def wrapper(self,*args,**opts):
+        def wrapper(self, *args, **opts):
             if self.is_redis_available():
-                return func(self,*args, **opts)
+                return func(self, *args,  **opts)
         return wrapper
-    
+
     def is_redis_available(self):
         try:
             self.r.ping()
@@ -38,16 +44,16 @@ class Selector_redis(object):
             return False
         return True
 
-    @_connected 
-    def gen_suggestWord(self,*data):
+    @_connected
+    def gen_suggestWord(self, *data):
         """gen_suggestWord: Generate decoded suggested word of a data set
         :data: list 
         :return: string
         """
         return (x.decode() for x in data)
 
-    @_connected 
-    def gen_fetchWords(self,key, withscore=False):
+    @_connected
+    def gen_fetchWords(self, key:str,  withscore=False):
         """gen_fetchWords: Fetch all suggested word of giving key from redis
             Sorted by hightest frequency and alphabetic order
         :key: string 'lang:{language}:gram:2:{word}'
@@ -57,11 +63,11 @@ class Selector_redis(object):
         if not self.r.exists(key):
             raise SelectorRedisNoBaseKeyFoundError("Error: key ", key, " does not exists. ")
         if not withscore:
-            return (x for x in self.r.zrevrange(key,0,-1))
+            return (x for x in self.r.zrevrange(key, 0, -1))
         else:
-            return (x for x in self.r.zrevrange(key,0,-1,'WITHSCORES'))
+            return (x for x in self.r.zrevrange(key, 0, -1, 'WITHSCORES'))
 
-    @_connected 
+    @_connected
     def addNewSuggestWord(self, key, word, freq=1):
         """addNewSuggestWord: Adding new suggested word to key
         :key: string
@@ -70,7 +76,7 @@ class Selector_redis(object):
         :returns: void
         :TODO: Validate  if word only contains Alpha str.isalpha()
         """
-        self.r.zadd(key,word,freq)
+        self.r.zadd(key, word, freq)
 
     @_connected
     def removeSuggestWord(self, key, word):
@@ -80,19 +86,20 @@ class Selector_redis(object):
         :returns: void
         :TODO: validation
         """
-        self.r.zrem(key,word)
+        self.r.zrem(key, word)
 
     @_connected
-    def existBaseKey(self,key):
+    def existBaseKey(self, key):
         """existBaseKey: Check if key exists
         :key: string
         :returns: bool
 
         """
         return self.r.exists(key)
-    
+
     @_connected
-    def increaseScoreSuggestedWord(self,key, suggestedWord, score=1):
+    @empty_arguments(SelectorRedisEmptyValue)
+    def increaseScoreSuggestedWord(self, key, suggestedWord, score=1):
         """increaseScoreSuggestedWord: Increase score of suggested word of giving key
         :key: String
         :suggestedWord: String
@@ -100,18 +107,19 @@ class Selector_redis(object):
         :returns: void
 
         """
-        #Test key exists
+        # Check if key or suggestedWord are empty
+        # Test key exists
         if not self.r.exists(key):
             raise SelectorRedisNoBaseKeyFoundError( "Error: key '{x}' does not exists.".format(x=key))
-        #Test suggested word
+        # Test suggested word
         fetch_collection = self.gen_fetchWords(key)
         suggestedWords = set(self.gen_suggestWord(*fetch_collection))
-        if not self.containing(suggestedWords,suggestedWord):
-            raise SelectorRedisNoSuggestWordFoundError("Error: suggested word: '{x}' does not exists with basekey '{y}'.".format(x=suggestedWord,y=key))
+        if not self.containing(suggestedWords, suggestedWord):
+            raise SelectorRedisNoSuggestWordFoundError("Error: suggested word: '{x}' does not exists with basekey '{y}'.".format(x=suggestedWord, y=key))
         if type(score) != int:
             raise TypeError("Error: '{x}' needs to be an int or a float.")
-        #Update
-        self.r.zincrby(key,suggestedWord,score)
+        # Update
+        self.r.zincrby(key, suggestedWord, score)
 
     def containing(self, collection, targetItem):
         """containing: Checks if a item is or is not in a collection of items
@@ -120,8 +128,3 @@ class Selector_redis(object):
         :returns: bool
         """
         return targetItem in collection
-
-class SelectorRedisError(Exception):    pass
-
-class SelectorRedisNoBaseKeyFoundError(SelectorRedisError):    pass
-class SelectorRedisNoSuggestWordFoundError(SelectorRedisError):    pass
