64156366c78ea70f9595daf3c3eb295cf899bce7
eronde <ghe@nemo-site.eu>
2017-10-21 18:02:36 +0200

    Modify tests/test_utils.py from unittest to pytest

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,25 +1,8 @@
-import unittest
-import re
-# from ..util.utils import *
 from py_word_suggest.utils import *
-
-class utilsTest(unittest.TestCase):
-    """Docstring for decorationsTest. """
-
-    def setUp(self):
-        """ decorators: setup
-        """
-        pass
-
-    def tearDown(self):
-        pass
-
-    def test_is_empty(self):
-        """utils, is_empty: Check if an object is empty or contains spaces
-        :returns: TODO
-        """
-        self.assertTrue(is_empty(''))
-
-
-if __name__ == '__main__':
-    unittest.main()
+import pytest
+def test_is_empty():
+    """utils, is_empty: Check if an object is empty or contains spaces
+    :returns: TODO
+    """
+    assert is_empty('') == True 
+    assert is_empty('test') == False 
