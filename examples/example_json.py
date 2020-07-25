7a1a4f0e48852179c992b70afeb35ac711454110
eronde <ghe@nemo-site.eu>
2020-07-25 20:19:32 +0200

    examples/example_json.py: updated

--- examples/example_json.py
+++ examples/example_json.py
@@ -12,23 +12,28 @@ Options:
 =======
 """
 from py_word_suggest.PWS_Selector_json import *
-from py_word_suggest.utils import load_data_from_json
+from py_word_suggest.utils import load_data_from_json, utilsError
 from docopt import docopt
+
+
 def main():
     fn = None
     arguments = docopt(__doc__, version='commandline 0.0.1')
     if arguments['-f']:
-       fn = arguments['-f']
-        
+        fn = arguments['-f']
+
     if fn is None:
         fn = './examples/example.json'
-    
+
     try:
         bigrams = load_data_from_json(fn)
-    except Exception as e:
-        raise e
+    except FileNotFoundError:
+        print("Error, '{}' does not exists. Please enter existing filename.".format(fn))
         exit(1)
-    
+    except utilsError:
+        print("Error, '{}' is not a valid json object. Please enter a valid json file.".format(fn))
+        exit(1)
+
     try:
         obj = PWS_selector_json(bigrams)
     except Exception as e:
@@ -36,17 +41,20 @@ def main():
         exit(1)
 
     if arguments['lookup']:
-       key = 'lang:{l}:0:{w}'.format(l=arguments['--lang'], w=arguments['--word'])
+        key = 'lang:{l}:0:{w}'.format(
+            l=arguments['--lang'], w=arguments['--word'])
     try:
         obj.set_suggestedWords(key)
     except Exception as e:
         print("{e}".format(e=e))
         exit(1)
 
-    print("'{w}' has the following suggested words:\n".format(w=arguments['--word']))
+    print("'{w}' has the following suggested words:\n".format(
+        w=arguments['--word']))
     print(list(obj._suggestWords))
     with open(fn, 'r') as f:
         obj._bigrams = f
-    
+
+
 if __name__ == "__main__":
     main()
