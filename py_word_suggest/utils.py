3bf7ba82f2b737211a4f08a63a57b9c86f52b92d
eronde <ghe@nemo-site.eu>
2020-07-25 15:08:01 +0200

    py_word_suggest/utils.py: load_data_from_data, check if file has an json object

--- py_word_suggest/utils.py
+++ py_word_suggest/utils.py
@@ -1,8 +1,13 @@
 # py_word_suggest/utils.py
-class utilsError(Exception): pass
-from json import dump, load, loads
-from . import export
+# from json import dump, load, loads
+import json
 from subprocess import PIPE, Popen
+from . import export
+
+
+class utilsError(Exception):
+    pass
+
 
 @export
 def is_empty(obj):
@@ -18,16 +23,18 @@ def is_empty(obj):
     else:
         return obj == '' or str(obj).isspace()
 
+
 def containing(collection, targetItem):
     """containing: Checks if a item is or is not in a collection of items
     :items: collection of items
-    :targetItem: 
+    :targetItem:
     :returns: bool
     """
-    if is_iterable(collection) and not isinstance(collection,str):
+    if is_iterable(collection) and not isinstance(collection, str):
         return targetItem in collection
     else:
         raise utilsError("Error: collection is not iterable or is a string")
+
 
 def is_iterable(obj):
     """is_empty: Check if object is iterable.
@@ -39,68 +46,84 @@ def is_iterable(obj):
     except TypeError as e:
         return False
 
+
 def load_data_from_json(filename):
-    """load_data_from_json: Load data from json file 
+    """load_data_from_json: Load data from json file
     :obj:
     :returns: dict
     """
     try:
         with open(filename, 'r', encoding='utf-8') as f:
-            return load(f)
-    except Exception as e:
-        print(e)
+            try:
+                return json.load(f)
+            except json.JSONDecodeError:
+                raise utilsError(
+                    "Error, load_data_from_json: '{}' needs to be a json object.".format(filename))
+            else:
+                return json.load(f)
+    except FileNotFoundError:
+        raise FileNotFoundError(
+            "Error, load_data_from_json: '{}' does not exists.".format(filename))
+
 
 def grep_jsonstring_from_system(key, filename):
     """grep_jsonstring_from_system: Get json string from json file, using jq ***JQ IS REQUIRED ON SYSTEM ***
-    :key: str, key  to find 
+    :key: str, key  to find
     :filename: file, json
     :returns: dict/null
     """
-    #Replace qoutes by double 
+    # Replace qoutes by double
     if isinstance(key, str):
-       if(key.startswith('"') and key.endswith('"')):
-           pass
-       else: raise utilsError("Error, grep_jsonstring_from_system: '{}' needs to be a str type and need to be between double quotes.".format(key)) #end check double quotes
+        if(key.startswith('"') and key.endswith('"')):
+            pass
+        else:
+            raise utilsError("Error, grep_jsonstring_from_system: '{}' needs to be a str type and need to be between double quotes.".format(
+                key))  # end check double quotes
     else:
-        raise utilsError("Error, grep_jsonstring_from_system: '{}' needs to be a str type and need to be between double quotes.".format(key))
-      
-    ###############file check
-    cmd = "cat {} | jq -jc '.{}'".format(filename,key)
+        raise utilsError(
+            "Error, grep_jsonstring_from_system: '{}' needs to be a str type and need to be between double quotes.".format(key))
+
+    # file check
+    cmd = "cat {} | jq -jc '.{}'".format(filename, key)
     try:
         with open(filename):
             jqp = Popen([cmd], stdout=PIPE, stderr=PIPE, shell=True)
-            jqr,err = jqp.communicate()
+            jqr, err = jqp.communicate()
     except IOError:
-         raise utilsError("Error, grep_jsonstring_from_system: File {} not exists or is busy.".format(filename))
-    #Return if pattern not exists
+        raise utilsError(
+            "Error, grep_jsonstring_from_system: File {} not exists or is busy.".format(filename))
+    # Return if pattern not exists
     if len(err) == 0 and jqr.decode() == 'null':
         return False
-   #Return error if grep produces an error 
+   # Return error if grep produces an error
     if len(err) != 0:
         raise utilsError("Error, grep_jsonstring_from_system: {}".format(err))
 
     # return json object
-    # check key between doubleqoutes 
+    # check key between doubleqoutes
     if not key.startswith('"'):
         key = '"'+key
-    else:pass #startswith
+    else:
+        pass  # startswith
     if not key.endswith('"'):
         key = key+'}'
-    else:pass #endswith
-    return loads('{'+'{}:{}'.format(key, jqr.decode())+'}')
+    else:
+        pass  # endswith
+    return json.loads('{'+'{}:{}'.format(key, jqr.decode())+'}')
+
 
 def load_json_string(jsonString):
     """load_json_string: Load json  object from string
-    :jsonString: str/bytes, string represetation of a json object, like {"key":"value"}. A str item needs to be set between double quotes 
+    :jsonString: str/bytes, string represetation of a json object, like {"key":"value"}. A str item needs to be set between double quotes
     :returns: dict
     """
-    hasBraces = 0 
+    hasBraces = 0
     if isinstance(jsonString, bytes):
         if jsonString.endswith(b'}') and jsonString.startswith(b'{'):
             hasBraces = 1
         # else:pass
     # else:pass
-    #Check if jsonString str is set between braces
+    # Check if jsonString str is set between braces
     if isinstance(jsonString, str):
         if jsonString.endswith('}') and jsonString.startswith('{'):
             hasBraces = 1
@@ -108,13 +131,16 @@ def load_json_string(jsonString):
     # else:pass
 
     if not hasBraces:
-            raise utilsError("Error load_json_string, jsonString, '{k}' needs to be a string represetation of a json object, jsonString needs to be set between braces. A str item needs to be set between double quotes.".format(k=jsonString))
-    # else:pass 
-    
+        raise utilsError(
+            "Error load_json_string, jsonString, '{k}' needs to be a string represetation of a json object, jsonString needs to be set between braces. A str item needs to be set between double quotes.".format(k=jsonString))
+    # else:pass
+
     if not isinstance(jsonString, str) and not isinstance(jsonString, bytes):
-        raise utilsError("Error load_json_string, jsonString, '{}' needs to be a string.".format(jsonString))                   
+        raise utilsError(
+            "Error load_json_string, jsonString, '{}' needs to be a string.".format(jsonString))
     # else:pass
     try:
-        return loads(jsonString)
+        return json.loads(jsonString)
     except Exception as e:
-            raise utilsError("Error load_json_string, jsonString, '{k}' needs to be string represetation of a json object, jsonString needs to be set between braces. A str item needs to be set between double quotes.".format(k=jsonString))
+        raise utilsError(
+            "Error load_json_string, jsonString, '{k}' needs to be string represetation of a json object, jsonString needs to be set between braces. A str item needs to be set between double quotes.".format(k=jsonString))
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,120 +1,171 @@
 from py_word_suggest.utils import *
-import pytest 
-# import pudb
+import pytest
 raw_json = """
-{"lang:nl:0:ben":[["ik", 22.0], ["er", 8.0], ["een", 7.0], ["je", 5.0]],"lang:nl:0:Ik":[["heb", 66.0], ["ben", 52.0], ["denk", 15.0], ["wil", 13.0], ["acht", 1.0]],
-"lang:eng:0:I":
-[["am", 100], ["want", 246], ["love", 999]],
-"lang:eng:0:am":
-[["the",100], ["Alice", 50],["Bob", 45]]}
+{"lang:nl:0:ben":[["ik", 22.0], ["er", 8.0], ["een", 7.0], ["je", 5.0]],"lang:nl:0:Ik":[["heb", 66.0], ["ben", 52.0], ["denk", 15.0], [
+    "wil", 13.0], ["acht", 1.0]],"lang:eng:0:I":[["am", 100], ["want", 246], ["love", 999]],"lang:eng:0:am":[["the",100], ["Alice", 50],["Bob", 45]]}
 """
+invalid_json = """
+test
+"""
+
+
+@pytest.fixture(scope="session")
+def invalid_json_file(tmpdir_factory):
+    fn = tmpdir_factory.mktemp("data_tmp").join("test_invalid.json")
+    fn.write(invalid_json)
+    invalid_json_fn = fn
+    return fn
 
 
 @pytest.fixture(scope="session")
 def raw_json_file(tmpdir_factory):
     f = tmpdir_factory.mktemp("data_tmp").join("test.json")
-
     f.write(raw_json)
     return f
 
+
+def setUp(invalid_json_file):
+    invalid_json_file
+
+
 @pytest.mark.parametrize("testInput, expectedOutput, state",
-            [
-                (b'{"lang:nl:0:Ik":[["heb", 66.0], ["ben", 52.0], ["denk", 15.0], ["wil", 13.0], ["acht", 1.0]]}', {'lang:nl:0:Ik':[['heb', 66.0], ['ben', 52.0], ['denk', 15.0], ['wil', 13.0], ['acht', 1.0]]}, 'normalState'),
-                ('{"lang:nl:0:Ik":[["heb", 66.0], ["ben", 52.0], ["denk", 15.0], ["wil", 13.0], ["acht", 1.0]]}', {'lang:nl:0:Ik':[['heb', 66.0], ['ben', 52.0], ['denk', 15.0], ['wil', 13.0], ['acht', 1.0]]}, 'normalState'),
-                ('"lang:nl"', "Error load_json_string, jsonString, '\"lang:nl\"' needs to be a string represetation of a json object, jsonString needs to be set between braces. A str item needs to be set between double quotes.", 'errorState'),
-                (b'"lang:nl"', "Error load_json_string, jsonString, 'b'\"lang:nl\"'' needs to be a string represetation of a json object, jsonString needs to be set between braces. A str item needs to be set between double quotes.", 'errorState'),
-               (b'\'lang\':0"', "Error load_json_string, jsonString, 'b'\\'lang\\':0\"'' needs to be a string represetation of a json object, jsonString needs to be set between braces. A str item needs to be set between double quotes.", 'errorState'),
-                (0, "Error load_json_string, jsonString, '0' needs to be a string represetation of a json object, jsonString needs to be set between braces. A str item needs to be set between double quotes.", 'errorState'),
-            ]
-        )
+                         [
+                             (b'{"lang:nl:0:Ik":[["heb", 66.0], ["ben", 52.0], ["denk", 15.0], ["wil", 13.0], ["acht", 1.0]]}', {
+                              'lang:nl:0:Ik': [['heb', 66.0], ['ben', 52.0], ['denk', 15.0], ['wil', 13.0], ['acht', 1.0]]}, 'normalState'),
+                             ('{"lang:nl:0:Ik":[["heb", 66.0], ["ben", 52.0], ["denk", 15.0], ["wil", 13.0], ["acht", 1.0]]}', {
+                              'lang:nl:0:Ik': [['heb', 66.0], ['ben', 52.0], ['denk', 15.0], ['wil', 13.0], ['acht', 1.0]]}, 'normalState'),
+                             ('"lang:nl"', "Error load_json_string, jsonString, '\"lang:nl\"' needs to be a string represetation of a json object, jsonString needs to be set between braces. A str item needs to be set between double quotes.", 'errorState'),
+                             (b'"lang:nl"', "Error load_json_string, jsonString, 'b'\"lang:nl\"'' needs to be a string represetation of a json object, jsonString needs to be set between braces. A str item needs to be set between double quotes.", 'errorState'),
+                             (b'\'lang\':0"', "Error load_json_string, jsonString, 'b'\\'lang\\':0\"'' needs to be a string represetation of a json object, jsonString needs to be set between braces. A str item needs to be set between double quotes.", 'errorState'),
+                             (0, "Error load_json_string, jsonString, '0' needs to be a string represetation of a json object, jsonString needs to be set between braces. A str item needs to be set between double quotes.", 'errorState'),
+                         ]
+                         )
 def test_load_json_from_string(testInput, expectedOutput, state):
-    """utils, Json from string""" 
-    #Test normal behavior  
+    """utils, Json from string"""
+    # Test normal behavior
     if state == 'normalState':
         assert load_json_string(testInput) == expectedOutput
-        
-    #Test expect error  
+
+    # Test expect error
     if state == 'errorState':
         with pytest.raises(utilsError) as e:
             load_json_string(testInput)
         assert str(e.value) == expectedOutput
 
+
 @pytest.mark.parametrize("testInput, expectedOutput, state",
-            [
-                ('\"lang:nl:0:Ik\"' ,{"lang:nl:0:Ik":[["heb", 66.0], ["ben", 52.0], ["denk", 15.0], ["wil", 13.0], ["acht", 1.0]]}, 'normalState'),
-                ('\"lang:nl:0:Ik' ,"Error, grep_jsonstring_from_system: '\"lang:nl:0:Ik' needs to be a str type and need to be between double quotes.",'errorState'),
-                ('lang:nl:0:Ik\"' ,"Error, grep_jsonstring_from_system: 'lang:nl:0:Ik\"' needs to be a str type and need to be between double quotes.",'errorState'),
-                ('lang:nl:0:Ik' ,"Error, grep_jsonstring_from_system: 'lang:nl:0:Ik' needs to be a str type and need to be between double quotes.",'errorState'),
-                (0 ,"Error, grep_jsonstring_from_system: '0' needs to be a str type and need to be between double quotes.",'errorState'),
-                ('\"NoKeyFound\"' ,False,'normalState'),
-                
-                ('\"NO-MATCH\"', False, 'normalState'),
-                ('\"NOEXISTINGFILE\"', "Error, grep_jsonstring_from_system: File NOEXISTINGFILE not exists or is busy.",'fileError'),
-                # ('lang:nl:0:Ik' ,b'"lang:nl:0:Ik":[["heb", 66.0], ["ben", 52.0], ["denk", 15.0], ["wil", 13.0], ["acht", 1.0]]','":.*]]','defaultArguments'),
-            ]
-        )
-def test_grep_jsonstring_from_system(raw_json_file,testInput, expectedOutput, state):
-    """utils, Grep bigram from file with system jq util""" 
-    #Test default argument
+                         [
+                             ('\"lang:nl:0:Ik\"', {"lang:nl:0:Ik": [["heb", 66.0], ["ben", 52.0], [
+                              "denk", 15.0], ["wil", 13.0], ["acht", 1.0]]}, 'normalState'),
+                             ('\"lang:nl:0:Ik', "Error, grep_jsonstring_from_system: '\"lang:nl:0:Ik' needs to be a str type and need to be between double quotes.", 'errorState'),
+                             ('lang:nl:0:Ik\"', "Error, grep_jsonstring_from_system: 'lang:nl:0:Ik\"' needs to be a str type and need to be between double quotes.", 'errorState'),
+                             ('lang:nl:0:Ik', "Error, grep_jsonstring_from_system: 'lang:nl:0:Ik' needs to be a str type and need to be between double quotes.", 'errorState'),
+                             (0, "Error, grep_jsonstring_from_system: '0' needs to be a str type and need to be between double quotes.", 'errorState'),
+                             ('\"NoKeyFound\"', False, 'normalState'),
+
+                             ('\"NO-MATCH\"', False, 'normalState'),
+                             ('\"NOEXISTINGFILE\"', "Error, grep_jsonstring_from_system: File NOEXISTINGFILE not exists or is busy.", 'fileError'),
+                             # ('lang:nl:0:Ik' ,b'"lang:nl:0:Ik":[["heb", 66.0], ["ben", 52.0], ["denk", 15.0], ["wil", 13.0], ["acht", 1.0]]','":.*]]','defaultArguments'),
+                         ]
+                         )
+def test_grep_jsonstring_from_system(raw_json_file, testInput, expectedOutput, state):
+    """utils, Grep bigram from file with system jq util"""
+    # Test default argument
     if state == 'fileError':
         raw_json_file = 'NOEXISTINGFILE'
         with pytest.raises(utilsError) as e:
             grep_jsonstring_from_system(testInput, raw_json_file)
         assert str(e.value) == expectedOutput
-    #Test normal behavior  
+    # Test normal behavior
     if state == 'normalState':
-       assert grep_jsonstring_from_system(testInput,raw_json_file) == expectedOutput
-        
-    #Test expect error  
+        assert grep_jsonstring_from_system(
+            testInput, raw_json_file) == expectedOutput
+
+    # Test expect error
     if state == 'errorState':
         # pudb.set_trace()
         with pytest.raises(utilsError) as e:
             grep_jsonstring_from_system(testInput, raw_json_file)
             # pudb.set_trace()
         assert str(e.value) == expectedOutput
-           
+
+
 @pytest.mark.parametrize("testInput,expected_output",
-        [
-            ('', True),
-            (None, True),
-            ('NonWwhiteSpaces', False),
-            ('String with white-space', True),
-            (10, False)
-       ]
-        )
-def test_is_empty(testInput,expected_output):
+                         [
+                             ('', True),
+                             (None, True),
+                             ('NonWwhiteSpaces', False),
+                             ('String with white-space', True),
+                             (10, False)
+                         ]
+                         )
+def test_is_empty(testInput, expected_output):
     """utils, is_empty: Check if an object is empty or contains spaces"""
     assert is_empty(testInput) == expected_output
 
+
 @pytest.mark.parametrize("testInput,expectedOutput",
-            [
-            ("String", True),
-            (['lol,lol2'], True),
-            (('lol','lol2'), True),
-            ({'lol','lol2'}, True),
-            (10, False),
-            (None, False)
-            ]
-        )
-def test_is_iterable(testInput,expectedOutput):
+                         [
+                             ("String", True),
+                             (['lol,lol2'], True),
+                             (('lol', 'lol2'), True),
+                             ({'lol', 'lol2'}, True),
+                             (10, False),
+                             (None, False)
+                         ]
+                         )
+def test_is_iterable(testInput, expectedOutput):
     """utils, is_iterable Check if an object is iterable"""
     assert is_iterable(testInput) == expectedOutput
 
+
 @pytest.mark.parametrize("testInput, collection, expectedOutput, errorState",
-            [
-            ('Love',['I', 'Love', 'python'], True, False),
-            ('love',['I', 'Love', 'python'],False, False),
-            ('',['I', 'Love', 'python'], False, False),
-            (None,['I', 'Love', 'python'], False, False),
-            (None,"String", "Error: collection is not iterable or is a string", True),
-            ('Love',8, "Error: collection is not iterable or is a string", True),('Love',None, "Error: collection is not iterable or is a string",True),
-            ]
-            )
+                         [
+                             ('Love', ['I', 'Love', 'python'], True, False),
+                             ('love', ['I', 'Love', 'python'], False, False),
+                             ('', ['I', 'Love', 'python'], False, False),
+                             (None, ['I', 'Love', 'python'], False, False),
+                             (None, "String",
+                              "Error: collection is not iterable or is a string", True),
+                             ('Love', 8, "Error: collection is not iterable or is a string", True), (
+                                 'Love', None, "Error: collection is not iterable or is a string", True),
+                         ]
+                         )
 def test_containing(testInput, collection,  expectedOutput, errorState):
     """utils: Check if collection contains an item"""
-    if  errorState is False:
+    if errorState is False:
         assert containing(collection, testInput) == expectedOutput
     else:
         with pytest.raises(utilsError) as e:
             containing(collection, testInput)
         assert str(e.value) == expectedOutput
+
+
+@pytest.mark.parametrize("expectedOutput, state",
+                         [
+                             ({"lang:nl:0:ben": [["ik", 22.0], ["er", 8.0], ["een", 7.0], ["je", 5.0]], "lang:nl:0:Ik":[["heb", 66.0], ["ben", 52.0], ["denk", 15.0], [
+                                 "wil", 13.0], ["acht", 1.0]], "lang:eng:0:I":[["am", 100], ["want", 246], ["love", 999]], "lang:eng:0:am":[["the", 100], ["Alice", 50], ["Bob", 45]]}, 'normalState'),
+                             ("Error, load_data_from_json: \'NOEXISTINGFILE\' does not exists.",
+                              'noFileExistState'),
+                             ("Error, load_data_from_json: '{}' needs to be a json object.",
+                              'invalidJsonState'),
+
+                         ]
+                         )
+def test_load_json_from_file(raw_json_file, invalid_json_file, expectedOutput, state):
+    """utils, load json data from file"""
+    # Test default argument
+    if state == 'noFileExistState':
+        raw_json_file = 'NOEXISTINGFILE'
+        with pytest.raises(FileNotFoundError) as e:
+            load_data_from_json(raw_json_file)
+        assert str(e.value) == expectedOutput
+    # Test normal behavior
+    if state == 'normalState':
+        assert load_data_from_json(raw_json_file) == expectedOutput
+
+    # Test invalid_json_file error
+    if state == 'invalidJsonState':
+        with pytest.raises(utilsError) as e:
+            load_data_from_json(str(invalid_json_file))
+        assert str(e.value) == expectedOutput.format(str(invalid_json_file))
